458bfce1aae4cf2354408e7b1d4a761200fcb32d
IRoot <74614800+imroot07@users.noreply.github.com>
2023-06-27 19:10:15 -0400

    created ArmSubsystem

---
+++ src/main/java/frc/robot/subsystems/arm/ArmSubsystem.java
@@ -0,0 +1,29 @@
+package frc.robot.subsystems.arm;
+
+import java.util.Set;
+
+import org.strykeforce.telemetry.measurable.MeasurableSubsystem;
+import org.strykeforce.telemetry.measurable.Measure;
+
+public class ArmSubsystem extends MeasurableSubsystem {
+    public ArmSubsystem() {}
+
+    @Override
+    public Set<Measure> getMeasures() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public enum ArmStates {
+        STOW,
+        FLOOR,
+        SHELF_CUBE,
+        SHELF_CONE,
+        SCORE,
+        TO_STOW,
+        TO_FLOOR,
+        TO_SHELF_CUBE,
+        TO_SHELF_CONE,
+        TO_SCORE,
+    }
+}
