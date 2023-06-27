ba1aea8e22d5243d536fc818b53c215bd761df10
IRoot <74614800+imroot07@users.noreply.github.com>
2023-06-27 18:44:59 -0400

    added periodic and transition funcitons

--- src/main/java/frc/robot/subsystems/robotState/RobotStateSubsystem.java
+++ src/main/java/frc/robot/subsystems/robotState/RobotStateSubsystem.java
@@ -3,16 +3,79 @@ package frc.robot.subsystems.robotState;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class RobotStateSubsystem extends SubsystemBase {
+  // private ArmSubsystem armSubsystem;
   private RobotStates robotState = RobotStates.STOW;
 
   public RobotStateSubsystem() {}
 
+  public void toStow() {
+    robotState = RobotStates.TO_STOW;
+  }
+
+  public void toFloorPickup() {
+    robotState = RobotStates.TO_FLOOR_PICKUP;
+  }
+
+  public void toShelfPickup(boolean isAuto) {
+    if (isAuto) { // FIXME: maybe do logic inside the function (eg. which direction we are facing)
+      robotState = RobotStates.TO_AUTO_SHELF;
+    } else {
+      robotState = RobotStates.TO_MANUAL_SHELF;
+    }
+  }
+
+  public void toScore() {
+    robotState = RobotStates.TO_SCORE;
+  }
+
+  public void toAutobalance() {
+    robotState = RobotStates.TO_AUTOBALANCE;
+  }
+
+  @Override
+  public void periodic() {
+    switch (robotState) {
+      case STOW:
+        break;
+      case FLOOR_PICKUP:
+        break;
+      case AUTO_SHELF:
+        break;
+      case MANUAL_SHELF:
+        break;
+      case SCORE:
+        break;
+      case AUTOBALANCE:
+        break;
+      case TO_STOW:
+        break;
+      case TO_FLOOR_PICKUP:
+        break;
+      case TO_AUTO_SHELF:
+        break;
+      case TO_MANUAL_SHELF:
+        break;
+      case TO_SCORE:
+        break;
+      case TO_AUTOBALANCE:
+        break;
+      default:
+        break;
+    }
+  }
+
   public enum RobotStates {
     STOW,
-    FLOOR,
+    FLOOR_PICKUP,
     AUTO_SHELF,
     MANUAL_SHELF,
-    MANUAL_SCORE,
+    SCORE,
     AUTOBALANCE,
+    TO_STOW,
+    TO_FLOOR_PICKUP,
+    TO_AUTO_SHELF,
+    TO_MANUAL_SHELF,
+    TO_SCORE,
+    TO_AUTOBALANCE,
   }
 }
