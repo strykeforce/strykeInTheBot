4dafad2be68106314474d2ebe517df774489c450
mwitcpalek <mwitcpalek@gmail.com>
2023-09-26 22:42:34 -0400

    fix file naming problems

--- src/main/java/frc/robot/commands/robotState/ClearGamePieceCommand.java
+++ src/main/java/frc/robot/commands/robotState/ClearGamePieceCommand.java
@@ -1,12 +1,12 @@
 package frc.robot.commands.robotState;
 
 import edu.wpi.first.wpilibj2.command.InstantCommand;
-import frc.robot.subsystems.robotState.RobotStateSubsystem;
+import frc.robot.subsystems.robotState.MinimalRobotStateSubsystem;
 
 public class ClearGamePieceCommand extends InstantCommand {
-  private RobotStateSubsystem robotStateSubsystem;
+  private MinimalRobotStateSubsystem robotStateSubsystem;
 
-  public ClearGamePieceCommand(RobotStateSubsystem robotStateSubsystem) {
+  public ClearGamePieceCommand(MinimalRobotStateSubsystem robotStateSubsystem) {
     this.robotStateSubsystem = robotStateSubsystem;
   }
 
